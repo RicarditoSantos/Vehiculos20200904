837722fc96c55a46e8eb748a9222e437fff96c96
Ricardo Santos <maravilla56@hotmail.com>
2022-10-31 21:36:31 +0100

    Iniciacion

---
+++ Data/Models/Vehiculo.cs
@@ -0,0 +1,10 @@
+namespace Vehiculos20200904.Data.Models;
+
+public class Vehiculo
+{
+    public int vehiculoID { get; set; }
+    public string? Marca { get; set; }
+    public string? Modelo { get; set; }
+    public double Año { get; set; }
+    public string? Color { get; set; }
+}
